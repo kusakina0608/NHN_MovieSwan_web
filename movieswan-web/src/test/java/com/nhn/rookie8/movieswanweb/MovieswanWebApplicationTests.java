53fadf6a028f87a2bb582fba13071d67a7fd199a
Supergrammer <supergrammer@naver.com>
2021-02-05 16:24:17 +0900

    [Feat] Initial Commit

---
+++ movieswan-web/src/test/java/com/nhn/rookie8/movieswanweb/MovieswanWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhn.rookie8.movieswanweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieswanWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
