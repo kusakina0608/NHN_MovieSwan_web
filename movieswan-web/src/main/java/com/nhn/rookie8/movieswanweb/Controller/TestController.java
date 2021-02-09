6402a3e8ee8afd6337a42c958e6fbf8f73586f61
supergrammer <supergrammer@naver.com>
2021-02-09 16:22:19 +0900

    [Feat] common 페이지 UI 개발, #신입-베이스캠프-8기-루키백조TF/30

---
+++ movieswan-web/src/main/java/com/nhn/rookie8/movieswanweb/Controller/TestController.java
@@ -0,0 +1,15 @@
+package com.nhn.rookie8.movieswanweb.Controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/layout")
+@Log4j2
+public class TestController {
+    @GetMapping("/common_page")
+    public void commonPage() {
+    }
+}
