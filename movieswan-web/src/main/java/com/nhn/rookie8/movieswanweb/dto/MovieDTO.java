9a94cbd40cda8210a2d21e9c26045b5df2aa7646
이대진 <daejin.lee@nhn.com>
2021-02-09 23:41:40 +0900

    [Feat] Movie DTO 생성 #신입-베이스캠프-8기-루키백조TF/62
    예매 페이지에서 영화 목록을 받아올 때 필요한 Movie DTO 생성

---
+++ movieswan-web/src/main/java/com/nhn/rookie8/movieswanweb/dto/MovieDTO.java
@@ -0,0 +1,21 @@
+package com.nhn.rookie8.movieswanweb.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder(toBuilder = true)
+public class MovieDTO {
+    private String mid;
+    private String name;
+    private String poster;
+    private String director;
+    private String actor;
+    private int runtime;
+    private String genre;
+    private String story;
+    private LocalDate startdate;
+    private LocalDate enddate;
+}
