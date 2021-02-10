5c9e84d94f84b7fdfaa737eb993afca2e8b6fe06
김현철 <supergrammer@naver.com>
2021-02-10 17:06:32 +0900

    [Chore] 프로젝트 설정, 디렉토리 구조 변경

---
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/MovieswanTicketAppApplication.java
@@ -0,0 +1,13 @@
+package com.nhn.rookie8.movieswanticketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieswanTicketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieswanTicketAppApplication.class, args);
+	}
+
+}
---
+++ movieswan-ticket-app/src/test/java/com/nhn/rookie8/movieswanticketapp/MovieswanTicketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhn.rookie8.movieswanticketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieswanTicketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
