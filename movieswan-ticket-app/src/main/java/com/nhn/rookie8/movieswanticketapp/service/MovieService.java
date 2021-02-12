c1fc9068009f327df8cf50373d8f4178352a020b
이대진 <daejin.lee@nhn.com>
2021-02-12 14:30:37 +0900

    [Feat] Movie Service 인터페이스 생성 #신입-베이스캠프-8기-루키백조TF/62
    영화 관련 API 호출을 위한 Movie Service 인터페이스 생성

--- movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/service/MovieService.java
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/service/MovieService.java
@@ -1,4 +1,9 @@
 package com.nhn.rookie8.movieswanticketapp.service;
 
+import com.nhn.rookie8.movieswanticketapp.dto.MovieDTO;
+
+import java.util.List;
+
 public interface MovieService {
+    List<MovieDTO> getReleaseMovieList();
 }
