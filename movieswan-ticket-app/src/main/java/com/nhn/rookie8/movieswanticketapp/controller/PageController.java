66a53290d1e5236df1def84a89e6417cbc16f084
이대진 <daejin.lee@nhn.com>
2021-02-12 15:06:55 +0900

    [Feat] 페이지 컨트롤러 수정 #신입-베이스캠프-8기-루키백조TF/10
    예매 페이지 응답 추가

--- movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/controller/PageController.java
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/controller/PageController.java
@@ -1,4 +1,29 @@
 package com.nhn.rookie8.movieswanticketapp.controller;
 
+import com.nhn.rookie8.movieswanticketapp.dto.MovieDTO;
+import com.nhn.rookie8.movieswanticketapp.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+@Log4j2
+@RequiredArgsConstructor
 public class PageController {
+    private final MovieService movieService;
+
+    @GetMapping("/booking")
+    public String booking(Model model){
+        log.info("booking");
+        List<MovieDTO> movieList = movieService.getReleaseMovieList();
+        model.addAttribute("movieList", movieList);
+        return "page/booking";
+    }
+
 }
