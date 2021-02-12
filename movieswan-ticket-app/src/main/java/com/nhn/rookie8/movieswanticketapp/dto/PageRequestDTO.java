3b14ec54710ffb3d8febc417c610aa09a117a722
박경원 <pkw1012@nhn.com>
2021-02-12 18:46:33 +0900

    [Feat] 영화 및 페이지 DTO 생성, #신입-베이스캠프-8기-루키백조TF/59

---
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/dto/PageRequestDTO.java
@@ -0,0 +1,16 @@
+package com.nhn.rookie8.movieswanticketapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@Builder
+@AllArgsConstructor
+@Data
+public class PageRequestDTO {
+    private int page;
+    private int size;
+}
---
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/dto/PageResultDTO.java
@@ -0,0 +1,26 @@
+package com.nhn.rookie8.movieswanticketapp.dto;
+
+import lombok.Data;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Data
+public class PageResultDTO<DTO, EN> {
+    private List<DTO> dtoList;
+
+    private int totalPage;
+
+    private int page;
+    private int size;
+
+    private int start, end;
+
+    private boolean prev, next;
+
+    private List<Integer> pageList;
+}
