0e5e57fb7bfae84c23bff3462bdc0328bad1c9b7
이대진 <daejin.lee@nhn.com>
2021-02-12 14:31:54 +0900

    [Feat] Movie Service 구현체 생성 #신입-베이스캠프-8기-루키백조TF/62
    영화 관련 API 호출을 위한 Movie Service 구현체 생성

---
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/service/MovieServiceImpl.java
@@ -0,0 +1,47 @@
+package com.nhn.rookie8.movieswanticketapp.service;
+
+import com.nhn.rookie8.movieswanticketapp.dto.MovieDTO;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Service
+public class MovieServiceImpl implements MovieService{
+    @Override
+    public List<MovieDTO> getReleaseMovieList() {
+        // TODO: Request to API to get data
+        // List<MovieDTO> movieList = ......
+
+        /**************************** for generating sample data ****************************/
+        List<MovieDTO> movieList = IntStream.rangeClosed(1, 20).asLongStream().mapToObj(i -> {
+            String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            StringBuilder salt = new StringBuilder();
+            Random rnd = new Random();
+            while (salt.length() < 4) {
+                int index = (int) (rnd.nextFloat() * alphabet.length());
+                salt.append(alphabet.charAt(index));
+            }
+            String saltStr = salt.toString();
+            MovieDTO dto = MovieDTO.builder()
+                    .mid(salt + "-" + String.format("%04d", i))
+                    .name("ツユ" + String.format("%4d", i))
+                    .poster("/asset/image/sample_movie.png")
+                    .director("Daejin-Lee")
+                    .actor("Hyerin-Yoo")
+                    .runtime(200)
+                    .genre("Romance")
+                    .story("하하호호 샘플 영화 스토리입니다~~~\n행복하게 살았읍니다~~")
+                    .startdate(LocalDate.now())
+                    .enddate(LocalDate.now().plusDays(14))
+                    .build();
+            return dto;
+        }).collect(Collectors.toList());
+        /**************************** for generating sample data ****************************/
+
+        return movieList;
+    }
+}
