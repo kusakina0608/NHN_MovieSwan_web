f75232453e833aa3b64be243baffa2c6c8085a6e
박경원 <pkw1012@nhn.com>
2021-02-12 18:47:11 +0900

    [Feat] 영화 Service 생성, #신입-베이스캠프-8기-루키백조TF/59

--- movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/service/MovieService.java
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/service/MovieService.java
@@ -1,4 +1,16 @@
 package com.nhn.rookie8.movieswanticketapp.service;
 
+import com.nhn.rookie8.movieswanticketapp.dto.MovieDTO;
+import com.nhn.rookie8.movieswanticketapp.dto.PageRequestDTO;
+import com.nhn.rookie8.movieswanticketapp.dto.PageResultDTO;
+import com.nhn.rookie8.movieswanticketapp.entity.Movie;
+
 public interface MovieService {
+    String register(MovieDTO movieDTO);
+
+    PageResultDTO<MovieDTO, Movie> getList(PageRequestDTO pageRequestDTO);
+
+    MovieDTO read(String mid);
+
+    void modify(MovieDTO movieDTO);
 }
