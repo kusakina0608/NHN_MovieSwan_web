cff15e062c9ed88be940919caa18907b2dd4ebfb
김현철 <supergrammer@naver.com>
2021-02-13 11:47:15 +0900

    [Feat] 영화 상영 시간표 DTO 생성, #신입-베이스캠프-8기-루키백조TF/74

---
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/dto/ScheduleDTO.java
@@ -0,0 +1,20 @@
+package com.nhn.rookie8.movieswanticketapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScheduleDTO {
+    private String mid;
+    private LocalDateTime time;
+    private String tid;
+
+    private LocalDateTime regDate, modDate;
+}
