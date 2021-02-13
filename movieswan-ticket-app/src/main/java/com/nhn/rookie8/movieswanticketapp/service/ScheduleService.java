08410b31125cf5fe3f0ce274ce880f64fd0886d8
김현철 <supergrammer@naver.com>
2021-02-13 12:08:00 +0900

    [Feat] 상영 시간표 인터페이스 생성, #신입-베이스캠프-8기-루키백조TF/74

---
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/service/ScheduleService.java
@@ -0,0 +1,7 @@
+package com.nhn.rookie8.movieswanticketapp.service;
+
+import com.nhn.rookie8.movieswanticketapp.dto.ScheduleDTO;
+
+public interface ScheduleService {
+    Long registerSchedule(ScheduleDTO dto);
+}
