f87036536faf9288d91eb75f2576cb9b25ea1927
박경원 <pkw1012@nhn.com>
2021-02-14 23:44:30 +0900

    [Feat] 웹상에서 영화 목록 출력, #신입-베이스캠프-8기-루키백조TF/59

--- movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/controller/PageController.java
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/controller/PageController.java
@@ -1,4 +1,31 @@
 package com.nhn.rookie8.movieswanticketapp.controller;
 
+import com.nhn.rookie8.movieswanticketapp.dto.PageRequestDTO;
+import com.nhn.rookie8.movieswanticketapp.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/page")
+@RequiredArgsConstructor
 public class PageController {
+    private final MovieService service;
+
+    @GetMapping("/main")
+    public String mainPage() {
+        return "/layout/main_page";
+    }
+
+    @GetMapping("/movie_list")
+    public void movieList(PageRequestDTO pageRequestDTO, Model model) {
+        model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/booking")
+    public String booking() {
+        return "/page/booking";
+    }
 }
