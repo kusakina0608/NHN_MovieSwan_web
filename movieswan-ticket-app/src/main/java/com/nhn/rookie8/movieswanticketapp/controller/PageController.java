e92798b0fb32bf834f865d0f82dd230480e5329d
이대진 <daejin.lee@nhn.com>
2021-02-15 12:25:03 +0900

    [Feat] 페이지 컨트롤러 수정 #신입-베이스캠프-8기-루키백조TF/10
    RequestMapping 변경 ("/page" -> "/")
    MovieService 변수 이름 변경(service -> movieService)
    booking() 기능 구현

--- movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/controller/PageController.java
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/controller/PageController.java
@@ -1,5 +1,6 @@
 package com.nhn.rookie8.movieswanticketapp.controller;
 
+import com.nhn.rookie8.movieswanticketapp.dto.MovieDTO;
 import com.nhn.rookie8.movieswanticketapp.dto.PageRequestDTO;
 import com.nhn.rookie8.movieswanticketapp.service.MovieService;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +9,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/page")
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class PageController {
-    private final MovieService service;
+    private final MovieService movieService;
 
     @GetMapping("/main")
     public String mainPage() {
@@ -21,11 +24,13 @@ public class PageController {
 
     @GetMapping("/movie_list")
     public void movieList(PageRequestDTO pageRequestDTO, Model model) {
-        model.addAttribute("result", service.getList(pageRequestDTO));
+        model.addAttribute("result", movieService.getList(pageRequestDTO));
     }
 
     @GetMapping("/booking")
-    public String booking() {
-        return "/page/booking";
+    public String booking(Model model) {
+        List<MovieDTO> movieList = movieService.getReleaseList();
+        model.addAttribute("movieList", movieList);
+        return "page/booking";
     }
 }
