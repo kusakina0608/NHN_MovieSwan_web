df691647b08bcb31823b12d66858ea5169cf2783
박경원 <pkw1012@nhn.com>
2021-02-15 20:59:37 +0900

    [Feat] 리뷰 DTO 생성, #신입-베이스캠프-8기-루키백조TF/77

---
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/dto/ReviewDTO.java
@@ -0,0 +1,23 @@
+package com.nhn.rookie8.movieswanticketapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ReviewDTO {
+    private String rid;
+    private String mid;
+    private String uid;
+    private int grade;
+    private String content;
+    private LocalDateTime regdate;
+    private LocalDateTime moddate;
+}
