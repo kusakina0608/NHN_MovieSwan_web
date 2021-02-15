7751cc8931cb467bb9e8dd42192a8ff7e5fb6cdf
supergrammer <supergrammer@naver.com>
2021-02-15 18:31:01 +0900

    [Feat] 상영 시간표 조회, 삭제, 생성 Impl 구현, #신입-베이스캠프-8기-루키백조TF/68

--- movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/service/MovieScheduleServiceImpl.java
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/service/MovieScheduleServiceImpl.java
@@ -1,5 +1,6 @@
 package com.nhn.rookie8.movieswanticketapp.service;
 
+import com.nhn.rookie8.movieswanticketapp.dto.MovieScheduleDTO;
 import com.nhn.rookie8.movieswanticketapp.dto.MovieScheduleInpDTO;
 import com.nhn.rookie8.movieswanticketapp.entity.MovieSchedule;
 import com.nhn.rookie8.movieswanticketapp.repository.MovieScheduleRepository;
@@ -7,16 +8,35 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @Log4j2
 @RequiredArgsConstructor
 public class MovieScheduleServiceImpl implements MovieScheduleService {
     private final MovieScheduleRepository repository;
 
-   @Override
-   public String registerMovieSchedule(MovieScheduleInpDTO dto) {
-       MovieSchedule entity = dtoToEntity(dto);
-       repository.save(entity);
-       return entity.getTid();
-   }
+    @Override
+    public void registerMovieSchedule(MovieScheduleInpDTO dto) {
+        MovieSchedule entity = dtoToEntity(dto);
+        repository.save(entity);
+    }
+
+    @Override
+    public void deleteMovieSchedule(String tid) {
+        repository.deleteById(tid);
+    }
+
+    @Override
+    public List<MovieScheduleDTO> getMovieSchedule(String mid) {
+        List<MovieSchedule> result = repository.findByMidOrderByTimeDesc(mid);
+        List<MovieScheduleDTO> scheduleList = new ArrayList<MovieScheduleDTO> ();
+
+        result.forEach(e -> {
+            scheduleList.add(entityToDTO(e));
+        });
+        return scheduleList;
+    }
 }
