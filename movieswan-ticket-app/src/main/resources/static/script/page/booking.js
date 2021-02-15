03507e673c4809960224a26836f4ea3656c79ddc
이대진 <daejin.lee@nhn.com>
2021-02-15 18:56:20 +0900

    [Feat] 예매 페이지 스크립트 수정 #신입-베이스캠프-8기-루키백조TF/62

--- movieswan-ticket-app/src/main/resources/static/script/page/booking.js
+++ movieswan-ticket-app/src/main/resources/static/script/page/booking.js
@@ -6,17 +6,21 @@
     const dayList = document.querySelector("#day-list");
     const timeList = document.querySelector("#time-list");
 
-    const movieListElementLink = movieList.querySelectorAll(".list-element-link");
-    const theaterListElementLink = theaterList.querySelectorAll(".list-element-link");
-    const dayListElementLink = dayList.querySelectorAll(".list-element-link");
-    const timeListElementLink = timeList.querySelectorAll(".list-element-link");
+    const movieLinkList = movieList.querySelectorAll(".list-element-link");
+    const theaterLinkList = theaterList.querySelectorAll(".list-element-link");
+    const dayLinkList = dayList.querySelectorAll(".list-element-link");
+    const timeLinkList = timeList.querySelectorAll(".list-element-link");
+
+    const nextButton = document.querySelector(".next-button");
+    console.log(nextButton);
 
     const requestTicketAPI = axios.create({
-        baseURL: "http://10.161.106.72:13306"
+        // baseURL: "http://10.161.106.78"
+        baseURL: "http://127.0.0.1:8080"
     });
-    
-    const bookingAPI = {
-        getShowingDate: (movie) => {
+
+    const scheduleAPI = {
+        getSchedules: (movie) => {
             return requestTicketAPI.get(`/api/schedule/${movie.mid}`);
         }
     }
@@ -41,8 +45,49 @@
         })
     };
 
-    selectElement(movieListElementLink, bookingAPI.getShowingDate);
-    selectElement(theaterListElementLink);
-    selectElement(dayListElementLink);
-    selectElement(timeListElementLink);
+    nextButton.addEventListener("click", e => {
+        e.preventDefault();
+
+        let form = document.createElement("form");
+
+        form.setAttribute("charset", "UTF-8");
+        form.setAttribute("method", "Post");
+        form.setAttribute("action", "/booking/seat");
+        
+        let selectedMovie = movieList.querySelector(".selected");
+        let selectedDay = dayList.querySelector(".selected");
+        let selectedTime = timeList.querySelector(".selected");
+
+        if(selectedMovie && selectedDay && selectedTime){
+            console.log("OK");
+            let movieInput = document.createElement("input");
+            movieInput.setAttribute("type", "hidden");
+            movieInput.setAttribute("name", "mid");
+            movieInput.setAttribute("value", selectedMovie.querySelector("div").innerHTML);
+            form.appendChild(movieInput);
+
+            let dateInput = document.createElement("input");
+            dateInput.setAttribute("type", "hidden");
+            dateInput.setAttribute("name", "date");
+            dateInput.setAttribute("value", selectedDay.innerHTML);
+            form.appendChild(dateInput);
+
+            let timeInput = document.createElement("input");
+            timeInput.setAttribute("type", "hidden");
+            timeInput.setAttribute("name", "time");
+            timeInput.setAttribute("value", selectedTime.innerHTML);
+            form.appendChild(timeInput);
+            document.body.appendChild(form);
+            console.log(form);
+            form.submit();
+        }
+        else {
+            console.log("NO");
+        }
+    })
+
+    selectElement(movieLinkList, scheduleAPI.getSchedules);
+    selectElement(theaterLinkList);
+    selectElement(dayLinkList);
+    selectElement(timeLinkList);
 }());
