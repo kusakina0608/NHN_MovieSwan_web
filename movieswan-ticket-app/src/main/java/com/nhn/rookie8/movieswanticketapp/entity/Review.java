06812960a20ad6d0bfa45a957aeeefef9ebb752f
박경원 <pkw1012@nhn.com>
2021-02-15 20:29:52 +0900

    [Feat] 리뷰 Entity 생성, #신입-베이스캠프-8기-루키백조TF/77

---
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/entity/Review.java
@@ -0,0 +1,40 @@
+package com.nhn.rookie8.movieswanticketapp.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Review {
+    @Id
+    @Column(name = "rid", length = 15, nullable = false)
+    private String rid;
+
+    @Column(name = "mid", length = 10, nullable = false)
+    private String mid;
+
+    @Column(name = "uid", length = 21, nullable = false)
+    private String uid;
+
+    @Column(name = "grade", nullable = false)
+    private int grade;
+
+    @Column(name = "content", length = 500, nullable = false)
+    private String content;
+
+    @Column(name = "regdate", nullable = false)
+    private LocalDateTime regdate;
+
+    @Column(name = "moddate", nullable = false)
+    private LocalDateTime moddate;
+
+}
