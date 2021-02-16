3f16337cc00bcc91478575e3053a27a161146c06
supergrammer <supergrammer@naver.com>
2021-02-16 10:37:08 +0900

    [Fix] Desc 를 Asc 로 수정, timetable 요청 시간 순 출력

--- movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/repository/MovieScheduleRepository.java
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/repository/MovieScheduleRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MovieScheduleRepository extends JpaRepository<MovieSchedule, String> {
-    List<MovieSchedule> findByMidOrderByTimeDesc(String mid);
+    List<MovieSchedule> findByMidOrderByTimeAsc(String mid);
 }
