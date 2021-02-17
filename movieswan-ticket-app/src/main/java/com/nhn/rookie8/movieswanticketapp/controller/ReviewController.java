800375e6a07532788232600ebbede29cb21cdc49
박경원 <pkw1012@nhn.com>
2021-02-18 00:43:09 +0900

    [Feat] 리뷰 등록 구현, #신입-베이스캠프-8기-루키백조TF/61

--- movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/controller/ReviewController.java
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/controller/ReviewController.java
@@ -3,17 +3,26 @@ package com.nhn.rookie8.movieswanticketapp.controller;
 import com.nhn.rookie8.movieswanticketapp.dto.ReviewDTO;
 import com.nhn.rookie8.movieswanticketapp.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/review")
+@Log4j2
 public class ReviewController {
     private final ReviewService service;
 
     @PostMapping("/register")
-    public void registerReview(ReviewDTO reviewDTO) {
+    public String registerReview(ReviewDTO reviewDTO, RedirectAttributes redirectAttributes) {
+        log.info(reviewDTO);
         service.register(reviewDTO);
+
+        redirectAttributes.addAttribute("mid", reviewDTO.getMid());
+
+        return "redirect:/movie/detail";
     }
 
     @PutMapping("/modify")
---
+++ movieswan-ticket-app/src/main/resources/static/script/page/movie_detail.js
@@ -0,0 +1,45 @@
+'use strict';
+
+(function() {
+    var submit = document.querySelector(".review_register");
+
+    submit.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        let form = document.createElement("form");
+
+        form.setAttribute("charset", "UTF-8");
+        form.setAttribute("method", "Post");
+        form.setAttribute("action", "/api/review/register");
+
+        let gradeInput = document.querySelector(".myreview").querySelector(".grade").querySelector("input:checked");
+        let contentInput = document.querySelector(".myreview").querySelector(".content");
+
+        let mid = document.createElement("input");
+        mid.setAttribute("type", "hidden");
+        mid.setAttribute("name", "mid");
+        mid.setAttribute("value", midInput);
+        form.appendChild(mid);
+
+        let uid = document.createElement("input");
+        uid.setAttribute("type", "hidden");
+        uid.setAttribute("name", "uid");
+        uid.setAttribute("value", "testuser");
+        form.appendChild(uid);
+
+        let grade = document.createElement("input");
+        grade.setAttribute("type", "hidden");
+        grade.setAttribute("name", "grade");
+        grade.setAttribute("value", gradeInput.value);
+        form.appendChild(grade);
+
+        let content = document.createElement("input");
+        content.setAttribute("type", "hidden");
+        content.setAttribute("name", "content");
+        content.setAttribute("value", contentInput.value);
+        form.appendChild(content);
+
+        document.body.appendChild(form);
+        form.submit();
+    })
+})();
