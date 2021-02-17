95b8e50a3365d44ee50f43ff6231ba7cdf550cf3
김현철 <supergrammer@naver.com>
2021-02-17 20:50:13 +0900

    [Feat] 1:1 문의 ResultDTO 생성, #신입-베이스캠프-8기-루키백조TF/55

---
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/dto/ApiResultDTO.java
@@ -0,0 +1,18 @@
+package com.nhn.rookie8.movieswanticketapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResultDTO {
+    private String httpStatus;
+    private Boolean isError;
+    private HashMap<Object, Object> data;
+}
