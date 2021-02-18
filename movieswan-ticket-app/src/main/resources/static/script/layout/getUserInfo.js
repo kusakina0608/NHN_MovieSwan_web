635beb93afbd7a7bb89bde6ca00b7c6d34c78a71
김현철 <supergrammer@naver.com>
2021-02-18 20:53:30 +0900

    [Feat] 로컬 스토리지 유저 정보 등록 구현

---
+++ movieswan-ticket-app/src/main/resources/static/script/layout/getUserInfo.js
@@ -0,0 +1,31 @@
+(function () {
+    const loggedin = document.getElementById("loggedin");
+    const notloggedin = document.getElementById("notloggedin");
+
+    if (localStorage.getItem("name") == "null") {
+        loggedin.style.display = "none"
+        notloggedin.style.display = "block"
+    } else {
+        loggedin.style.display = "block"
+        notloggedin.style.display = "none"
+
+        const username = document.getElementById("user_name");
+        username.innerText = localStorage.getItem("name");
+    }
+})();
+
+const logoutOnClick = function () {
+    localStorage.setItem("uid", "null");
+    localStorage.setItem("name", "null");
+
+    (function (name) {
+        name = new RegExp(name + '=([^;]*)');
+        return name.test(document.cookie) ? unescape(RegExp.$1) : '';
+    });
+
+    const setCookie = function (name, value) {
+        document.cookie = name + '=' + escape(value);
+    }
+
+    setCookie("JSESSIONID", '');
+}   
