ac14f5d4549004ce3ea906befdbd209eae9f9b4c
이대진 <daejin.lee@nhn.com>
2021-02-18 11:53:00 +0900

    [Feat] seat 업데이트 기능 개발 #신입-베이스캠프-8기-루키백조TF/50

--- movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/repository/SeatRepository.java
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/repository/SeatRepository.java
@@ -4,7 +4,16 @@ import com.nhn.rookie8.movieswanticketapp.entity.Movie;
 import com.nhn.rookie8.movieswanticketapp.entity.Seat;
 import com.nhn.rookie8.movieswanticketapp.entity.SeatId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
 
 public interface SeatRepository extends JpaRepository<Seat, SeatId>, QuerydslPredicateExecutor<Seat> {
+    @Transactional
+    @Modifying
+    @Query("update Seat s set s.rid = :rid where s.tid = :tid AND s.sid = :sid AND s.uid = :uid ")
+    int updateRid(@Param("rid") String rid, @Param("tid") String tid, @Param("sid") String sid, @Param("uid") String uid);
 }
