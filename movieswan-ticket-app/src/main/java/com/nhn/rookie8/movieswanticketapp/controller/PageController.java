dc412a53f06bdb3feb43c93d439bdc9c4e9f9c3c
박경원 <pkw1012@nhn.com>
2021-02-19 14:59:35 +0900

    [Feat] MyReview에 영화 제목 및 링크 추가

--- movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/controller/PageController.java
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/controller/PageController.java
@@ -244,8 +244,17 @@ public class PageController {
             return "redirect:/user/login";
         } else {
             String uid = session.getAttribute("uid").toString();
+            PageResultDTO<ReviewDTO, Review> resultDTO = reviewService.findMyReviews(pageRequestDTO, uid);
+            List<ReviewDTO> reviewList = resultDTO.getDtoList();
+            HashMap<String, String> titleMap = new HashMap<String, String>();
+            reviewList.forEach(reviewDTO -> {
+                String title = movieService.read(reviewDTO.getMid()).getName();
+                titleMap.put(reviewDTO.getMid(), title);
+            });
+
             model.addAttribute("uid", session.getAttribute("uid"));
-            model.addAttribute("result", reviewService.findMyReviews(pageRequestDTO, uid));
+            model.addAttribute("result", resultDTO);
+            model.addAttribute("titleMap", titleMap);
             return "page/my_page_myreview";
         }
     }
