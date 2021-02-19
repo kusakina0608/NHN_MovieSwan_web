51a94b98639b6b2ab07a02ad8d13ec01bc4c2ccd
김현철 <supergrammer@naver.com>
2021-02-19 15:15:18 +0900

    [Feat] 내 예매 내역 조회 api 등록, #신입-베이스캠프-8기-루키백조TF/46

--- movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/service/ReservationService.java
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/service/ReservationService.java
@@ -3,13 +3,17 @@ package com.nhn.rookie8.movieswanticketapp.service;
 import com.nhn.rookie8.movieswanticketapp.dto.PageRequestDTO;
 import com.nhn.rookie8.movieswanticketapp.dto.PageResultDTO;
 import com.nhn.rookie8.movieswanticketapp.dto.ReservationDTO;
+import com.nhn.rookie8.movieswanticketapp.dto.ReservationResultDTO;
 import com.nhn.rookie8.movieswanticketapp.entity.Reservation;
+
+import java.time.LocalDateTime;
 
 public interface ReservationService {
     String createReservationId();
     String register(ReservationDTO dto);
 
     PageResultDTO<ReservationDTO, Reservation> getList(PageRequestDTO requestDTO);
+    PageResultDTO<ReservationResultDTO, Reservation> getMypageList(PageRequestDTO requestDTO);
 
     default Reservation dtoToEntity(ReservationDTO dto){
         Reservation entity = Reservation.builder()
@@ -39,4 +43,23 @@ public interface ReservationService {
                 .build();
         return dto;
     }
+
+    default ReservationResultDTO entityToDto(Reservation entity, String mid, String movieName, LocalDateTime startDate){
+        ReservationResultDTO dto = ReservationResultDTO.builder()
+                .rid(entity.getRid())
+                .tid(entity.getTid())
+                .uid(entity.getUid())
+                .childNum(entity.getChildNum())
+                .adultNum(entity.getAdultNum())
+                .oldNum(entity.getOldNum())
+                .totalNum(entity.getTotalNum())
+                .price(entity.getPrice())
+                .regDate(entity.getRegDate())
+                .mid(mid)
+                .movieName(movieName)
+                .startDate(startDate)
+                .build();
+        return dto;
+    }
+
 }
--- movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/service/ReservationServiceImpl.java
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/service/ReservationServiceImpl.java
@@ -3,8 +3,10 @@ package com.nhn.rookie8.movieswanticketapp.service;
 import com.nhn.rookie8.movieswanticketapp.dto.PageRequestDTO;
 import com.nhn.rookie8.movieswanticketapp.dto.PageResultDTO;
 import com.nhn.rookie8.movieswanticketapp.dto.ReservationDTO;
+import com.nhn.rookie8.movieswanticketapp.dto.ReservationResultDTO;
 import com.nhn.rookie8.movieswanticketapp.entity.QReservation;
 import com.nhn.rookie8.movieswanticketapp.entity.Reservation;
+import com.nhn.rookie8.movieswanticketapp.repository.MovieRepository;
 import com.nhn.rookie8.movieswanticketapp.repository.ReservationRepository;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
@@ -15,6 +17,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -77,6 +80,29 @@ public class ReservationServiceImpl implements ReservationService{
         BooleanBuilder booleanBuilder = getMyList(requestDTO);
         Page<Reservation> result = repository.findAll(booleanBuilder, pageable);
         Function<Reservation, ReservationDTO> fn = (entity -> entityToDto(entity));
+        return new PageResultDTO<>(result, fn);
+    }
+
+    @Override
+    public PageResultDTO<ReservationResultDTO, Reservation> getMypageList(PageRequestDTO requestDTO) {
+        Pageable pageable = requestDTO.getPageable(Sort.by("regDate").descending());
+        BooleanBuilder booleanBuilder = getMyList(requestDTO);
+        Page<Reservation> result = repository.findAll(booleanBuilder, pageable);
+
+        Function<Reservation, ReservationResultDTO> fn = (entity -> {
+            String mid = repository.getMovieMid(entity.getTid());
+            String name = repository.getMovieName(entity.getTid());
+
+            String tid = entity.getTid();
+            LocalDateTime startDate = LocalDateTime.of(
+                    2000 + Integer.parseInt(tid.substring(3, 5)),
+                    Integer.parseInt(tid.substring(5, 7)),
+                    Integer.parseInt(tid.substring(7, 9)),
+                    Integer.parseInt(tid.substring(9, 11)),
+                    Integer.parseInt(tid.substring(11, 13)));
+
+            return entityToDto(entity, mid, name, startDate);
+        });
         return new PageResultDTO<>(result, fn);
     }
 
