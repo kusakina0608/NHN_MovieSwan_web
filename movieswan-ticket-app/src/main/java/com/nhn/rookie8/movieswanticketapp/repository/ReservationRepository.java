40db3b3cfd5ce7103eb83f5d32acfddb3aa0048e
김현철 <supergrammer@naver.com>
2021-02-19 15:16:18 +0900

    [Feat] tid 로 mid, 영화 제목을 검색하는 쿼리 추가, #신입-베이스캠프-8기-루키백조TF/46

--- movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/repository/ReservationRepository.java
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/repository/ReservationRepository.java
@@ -2,7 +2,14 @@ package com.nhn.rookie8.movieswanticketapp.repository;
 
 import com.nhn.rookie8.movieswanticketapp.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
 public interface ReservationRepository extends JpaRepository<Reservation, String>, QuerydslPredicateExecutor<Reservation> {
+    @Query(value = "SELECT mid FROM movie WHERE mid IN (SELECT DISTINCT mid FROM movie_schedule as ms, reservation as rs WHERE ms.tid = rs.tid);", nativeQuery = true)
+    String getMovieMid(@Param("tid") String tid);
+
+    @Query(value = "SELECT name FROM movie WHERE mid IN (SELECT DISTINCT mid FROM movie_schedule as ms, reservation as rs WHERE ms.tid = rs.tid);", nativeQuery = true)
+    String getMovieName(@Param("tid") String tid);
 }
