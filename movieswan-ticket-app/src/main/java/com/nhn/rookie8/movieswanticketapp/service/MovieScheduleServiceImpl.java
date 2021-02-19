6c5b5896e9ab98ed2c40d426c14a59014cdac67c
김현철 <supergrammer@naver.com>
2021-02-19 15:15:42 +0900

    [Feat] FindMySchedule 메소드 추가, #신입-베이스캠프-8기-루키백조TF/46

--- movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/service/MovieScheduleServiceImpl.java
+++ movieswan-ticket-app/src/main/java/com/nhn/rookie8/movieswanticketapp/service/MovieScheduleServiceImpl.java
@@ -3,6 +3,7 @@ package com.nhn.rookie8.movieswanticketapp.service;
 import com.nhn.rookie8.movieswanticketapp.dto.MovieScheduleDTO;
 import com.nhn.rookie8.movieswanticketapp.dto.MovieScheduleInpDTO;
 import com.nhn.rookie8.movieswanticketapp.entity.MovieSchedule;
+import com.nhn.rookie8.movieswanticketapp.entity.Question;
 import com.nhn.rookie8.movieswanticketapp.repository.MovieScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -22,6 +23,12 @@ public class MovieScheduleServiceImpl implements MovieScheduleService {
     public void registerMovieSchedule(MovieScheduleInpDTO dto) {
         MovieSchedule entity = dtoToEntity(dto);
         repository.save(entity);
+    }
+
+    @Override
+    public MovieScheduleDTO findMovieSchedule(String tid) {
+        Optional<MovieSchedule> result = repository.findById(tid);
+        return result.isPresent() ? entityToDTO(result.get()) : null;
     }
 
     @Override
