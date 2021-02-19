0c4ce55ee91f1862d029fbcf6b2c650260a5a8c0
이대진 <daejin.lee@nhn.com>
2021-02-19 10:35:50 +0900

    [Feat] 엔드포인트 변경 #신입-베이스캠프-8기-루키백조TF/10

--- movieswan-ticket-app/src/main/resources/static/script/page/seat.js
+++ movieswan-ticket-app/src/main/resources/static/script/page/seat.js
@@ -17,8 +17,7 @@
     var selected = 0;
 
     const requestTicketAPI = axios.create({
-        // baseURL: "http://10.161.106.78"
-        baseURL: "http://127.0.0.1:8080"
+        baseURL: "http://movieswan.nhnent.com:8080"
     });
 
     // 상영시간표 API에 요청
