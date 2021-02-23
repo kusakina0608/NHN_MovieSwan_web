ac77d6b0555b121195c1bd5ede60440977f02b29
김현철 <supergrammer@naver.com>
2021-02-23 20:36:56 +0900

    [Refactor] 예약 조회 서비스 수정, #신입-베이스캠프-8기-루키백조TF/89

--- src/main/java/com/nhn/rookie8/movieswanticketapp/service/ReservationService.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/service/ReservationService.java
@@ -3,19 +3,15 @@ package com.nhn.rookie8.movieswanticketapp.service;
 import com.nhn.rookie8.movieswanticketapp.dto.PageRequestDTO;
 import com.nhn.rookie8.movieswanticketapp.dto.PageResultDTO;
 import com.nhn.rookie8.movieswanticketapp.dto.ReservationDTO;
-import com.nhn.rookie8.movieswanticketapp.dto.ReservationResultDTO;
 import com.nhn.rookie8.movieswanticketapp.entity.Reservation;
-
-import java.time.LocalDateTime;
 
 public interface ReservationService {
     String createReservationId();
     String register(ReservationDTO dto);
     String delete(ReservationDTO dto);
 
-    PageResultDTO<ReservationDTO, Reservation> getList(PageRequestDTO requestDTO);
-    PageResultDTO<ReservationResultDTO, Reservation> getMypageList(PageRequestDTO requestDTO, String uid);
-    ReservationResultDTO readReservation(String rid);
+    PageResultDTO<ReservationDTO, Reservation> getMyReservationList(PageRequestDTO requestDTO, String uid);
+    ReservationDTO getReservation(String rid);
 
     default Reservation dtoToEntity(ReservationDTO dto){
         Reservation entity = Reservation.builder()
@@ -32,7 +28,7 @@ public interface ReservationService {
     }
 
     default ReservationDTO entityToDto(Reservation entity){
-        ReservationDTO dto = ReservationDTO.builder()
+        ReservationDTO reservationDTO = ReservationDTO.builder()
                 .rid(entity.getRid())
                 .tid(entity.getTid())
                 .uid(entity.getUid())
@@ -43,26 +39,6 @@ public interface ReservationService {
                 .price(entity.getPrice())
                 .regDate(entity.getRegDate())
                 .build();
-        return dto;
+        return reservationDTO;
     }
-
-    default ReservationResultDTO entityToDto(Reservation entity, String mid, String movieName, String imagePath, LocalDateTime startDate){
-        ReservationResultDTO dto = ReservationResultDTO.builder()
-                .rid(entity.getRid())
-                .tid(entity.getTid())
-                .uid(entity.getUid())
-                .childNum(entity.getChildNum())
-                .adultNum(entity.getAdultNum())
-                .oldNum(entity.getOldNum())
-                .totalNum(entity.getTotalNum())
-                .price(entity.getPrice())
-                .regDate(entity.getRegDate())
-                .mid(mid)
-                .movieName(movieName)
-                .imgPath(imagePath)
-                .startDate(startDate)
-                .build();
-        return dto;
-    }
-
 }
