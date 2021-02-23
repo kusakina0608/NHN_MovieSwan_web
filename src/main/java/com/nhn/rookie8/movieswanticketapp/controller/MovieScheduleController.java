6dd69d39c7cf9ef6bb3fbbfbeeb78c5106bfe985
김현철 <supergrammer@naver.com>
2021-02-23 15:24:29 +0900

    [Refactor] 컨트롤러 예외 처리 추가, #신입-베이스캠프-8기-루키백조TF/10

--- src/main/java/com/nhn/rookie8/movieswanticketapp/controller/MovieScheduleController.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/controller/MovieScheduleController.java
@@ -16,61 +16,102 @@ import java.util.*;
 @RequestMapping("/api/schedule")
 
 @RequiredArgsConstructor
-@ResponseBody
 
 @Log4j2
 public class MovieScheduleController {
     private final MovieScheduleService service;
 
     @PostMapping("/register")
-    public void registerMovieSchedule(MovieScheduleInputDTO movieScheduleInputDTO) {
-        service.registerMovieSchedule(movieScheduleInputDTO);
+    public String registerMovieSchedule(MovieScheduleInputDTO movieScheduleInputDTO) {
+        try {
+            log.info("Request Input DTO : {}", movieScheduleInputDTO);
+            service.registerMovieSchedule(movieScheduleInputDTO);
+            return "redirect:/admin";
+        } catch (Exception e) {
+            log.error(e);
+            return null;
+        }
     }
 
     @DeleteMapping("/delete")
-    public void deleteMovieSchedule(String tid) {
-        service.deleteMovieSchedule(tid);
+    public String deleteMovieSchedule(String tid) {
+        try {
+            log.info("Request TID : {}", tid);
+            service.deleteMovieSchedule(tid);
+            return "redirect:/admin";
+        } catch (Exception e) {
+            log.error(e);
+            return null;
+        }
     }
 
     @GetMapping("/get")
+    @ResponseBody
     public MovieScheduleDTO getASchedule(@RequestParam String tid) {
-        return service.getASchedule(tid);
+        try {
+            log.info("Request TID : {}", tid);
+            MovieScheduleDTO result = service.getASchedule(tid);
+
+            if (result == null)
+                throw new Exception("No results were found for your search.");
+
+            return service.getASchedule(tid);
+        } catch (Exception e) {
+            log.error(e);
+            return null;
+        }
     }
 
     @GetMapping("/getall")
+    @ResponseBody
     // TODO: 컬렉션 반환값을 클래스로 처리하는 과정이 필요합니다. 추후 논의...
     public List<LinkedHashMap<String, List<String>>> getAllSchedulesOfMovie(@RequestParam String mid) {
-        List<MovieScheduleDTO> result = service.getAllSchedulesOfMovie(mid);
+        try {
+            log.info("Request MID : {}", mid);
+            List<MovieScheduleDTO> result = service.getAllSchedulesOfMovie(mid);
+            if (result.isEmpty())
+                throw new Exception("No results were found for your search.");
 
-        List<LinkedHashMap<String, List<String>>> scheduleList = new ArrayList<>();
-        LinkedHashMap<String, List<String>> linkedHashMap = new LinkedHashMap<>();
+            List<LinkedHashMap<String, List<String>>> scheduleList = new ArrayList<>();
+            LinkedHashMap<String, List<String>> linkedHashMap = new LinkedHashMap<>();
 
-        result.forEach(e -> {
-            String[] datetime = e.getTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")).split(" ");
-            String date = datetime[0];
-            String time = datetime[1];
+            result.forEach(e -> {
+                String[] datetime = e.getTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")).split(" ");
+                String date = datetime[0];
+                String time = datetime[1];
 
-            if (!linkedHashMap.containsKey(date)) {
-                List<String> list = new ArrayList<>();
-                linkedHashMap.put(date, list);
-            }
+                if (!linkedHashMap.containsKey(date)) {
+                    List<String> list = new ArrayList<>();
+                    linkedHashMap.put(date, list);
+                }
 
-            StringBuilder str = new StringBuilder();
-            str.append(time).append(' ').append(e.getTid());
-            linkedHashMap.get(date).add(str.toString());
-        });
-        scheduleList.add(linkedHashMap);
+                StringBuilder str = new StringBuilder();
+                str.append(time).append(' ').append(e.getTid());
+                linkedHashMap.get(date).add(str.toString());
+            });
+            scheduleList.add(linkedHashMap);
 
-        return scheduleList;
+            return scheduleList;
+        } catch (Exception e) {
+            log.error(e);
+            return null;
+        }
     }
 
     @GetMapping("/getlist")
+    @ResponseBody
     public List<MovieScheduleDTO> getAllSchedulesListOfMovie(@RequestParam String mid) {
-        List<MovieScheduleDTO> scheduleDTOList = service.getAllSchedulesOfMovie(mid);
-        return service.getAllSchedulesOfMovie(mid);
+        try {
+            log.info("Request MID : {}", mid);
+            List<MovieScheduleDTO> scheduleDTOList = service.getAllSchedulesOfMovie(mid);
+
+            if (scheduleDTOList.isEmpty())
+                throw new Exception("No results were found for your search.");
+
+            return service.getAllSchedulesOfMovie(mid);
+        } catch (Exception e) {
+            log.error(e);
+            return null;
+        }
     }
-
-
-
-
 }
