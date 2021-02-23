04f70528198e04a628498679833dc3c5edf7afff
김현철 <supergrammer@naver.com>
2021-02-23 13:22:39 +0900

    [Refactor] 시간표 컨트롤러 수정, #신입-베이스캠프-8기-루키백조TF/10

--- src/main/java/com/nhn/rookie8/movieswanticketapp/controller/MovieScheduleController.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/controller/MovieScheduleController.java
@@ -1,83 +1,75 @@
 package com.nhn.rookie8.movieswanticketapp.controller;
 
-import com.nhn.rookie8.movieswanticketapp.dto.MovieDTO;
 import com.nhn.rookie8.movieswanticketapp.dto.MovieScheduleDTO;
-import com.nhn.rookie8.movieswanticketapp.dto.MovieScheduleInpDTO;
-import com.nhn.rookie8.movieswanticketapp.entity.MovieSchedule;
-import com.nhn.rookie8.movieswanticketapp.response.MovieScheduleResponse;
+import com.nhn.rookie8.movieswanticketapp.dto.MovieScheduleInputDTO;
 import com.nhn.rookie8.movieswanticketapp.service.MovieScheduleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
-@RequestMapping("/api/schedule")
-@Log4j2
-@RequiredArgsConstructor
 @Controller
+@RequestMapping("/api/schedule")
+
+@RequiredArgsConstructor
 @ResponseBody
+
+@Log4j2
 public class MovieScheduleController {
     private final MovieScheduleService service;
 
     @PostMapping("/register")
-    public String registerSchd(MovieScheduleInpDTO inpDTO) {
-
-        service.registerMovieSchedule(inpDTO);
-
-        return "redirect:/admin";
+    public void registerMovieSchedule(MovieScheduleInputDTO movieScheduleInputDTO) {
+        service.registerMovieSchedule(movieScheduleInputDTO);
     }
 
     @DeleteMapping("/delete")
-    public String deleteSchd(String tid) {
-
+    public void deleteMovieSchedule(String tid) {
         service.deleteMovieSchedule(tid);
-
-        return "redirect:/admin";
     }
 
-    @GetMapping("/timetable")
-    public MovieScheduleResponse findSchd(@RequestParam String mid) {
-        List<MovieScheduleDTO> scheduleDTOList = service.getMovieSchedule(mid);
-        List<Map<String, List<String>>> scheduleList = new ArrayList<>();
-        Map<String, List<String>> map = new HashMap<>();
+    @GetMapping("/get")
+    public MovieScheduleDTO getASchedule(@RequestParam String tid) {
+        return service.getASchedule(tid);
+    }
 
-        scheduleDTOList.forEach(e -> {
+    @GetMapping("/getall")
+    public List<LinkedHashMap<String, List<String>>> getAllSchedulesOfMovie(@RequestParam String mid) {
+        List<MovieScheduleDTO> result = service.getAllSchedulesOfMovie(mid);
+
+        List<LinkedHashMap<String, List<String>>> scheduleList = new ArrayList<>();
+        LinkedHashMap<String, List<String>> linkedHashMap = new LinkedHashMap<>();
+
+        result.forEach(e -> {
             String[] datetime = e.getTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")).split(" ");
             String date = datetime[0];
             String time = datetime[1];
 
-            if (!map.containsKey(date)) {
-                List<String> list = new ArrayList<String>();
-                map.put(date, list);
+            if (!linkedHashMap.containsKey(date)) {
+                List<String> list = new ArrayList<>();
+                linkedHashMap.put(date, list);
             }
-            map.get(date).add(time + " " + e.getTid());
+
+            StringBuilder str = new StringBuilder();
+            str.append(time).append(' ').append(e.getTid());
+            linkedHashMap.get(date).add(str.toString());
         });
-        scheduleList.add(map);
+        scheduleList.add(linkedHashMap);
 
-        MovieScheduleResponse movieScheduleResponse = MovieScheduleResponse.builder()
-                .errorOccured(false)
-                .data(scheduleList)
-                .build();
-
-        return movieScheduleResponse;
+        return scheduleList;
     }
 
-    @GetMapping("/find")
-    public MovieScheduleDTO getOneSchd(@RequestParam String tid) {
-        return service.findMovieSchedule(tid);
+    @GetMapping("/getlist")
+    public List<MovieScheduleDTO> getAllSchedulesListOfMovie(@RequestParam String mid) {
+        List<MovieScheduleDTO> scheduleDTOList = service.getAllSchedulesOfMovie(mid);
+        return service.getAllSchedulesOfMovie(mid);
     }
 
-    @GetMapping("/get")
-    public List<MovieScheduleDTO> getSchd(@RequestParam String mid) {
-        List<MovieScheduleDTO> scheduleDTOList = service.getMovieSchedule(mid);
-        return service.getMovieSchedule(mid);
-    }
+
+
+
 }
