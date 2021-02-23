4e88f0bd63819e3ebc187213867b38045707bdfe
김현철 <supergrammer@naver.com>
2021-02-23 13:22:24 +0900

    [Refactor] 시간표 서비스 수정, #신입-베이스캠프-8기-루키백조TF/10

--- src/main/java/com/nhn/rookie8/movieswanticketapp/service/MovieScheduleServiceImpl.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/service/MovieScheduleServiceImpl.java
@@ -1,17 +1,21 @@
 package com.nhn.rookie8.movieswanticketapp.service;
 
 import com.nhn.rookie8.movieswanticketapp.dto.MovieScheduleDTO;
-import com.nhn.rookie8.movieswanticketapp.dto.MovieScheduleInpDTO;
+import com.nhn.rookie8.movieswanticketapp.dto.MovieScheduleInputDTO;
+import com.nhn.rookie8.movieswanticketapp.dto.PageResultDTO;
+import com.nhn.rookie8.movieswanticketapp.dto.ReservationResultDTO;
 import com.nhn.rookie8.movieswanticketapp.entity.MovieSchedule;
-import com.nhn.rookie8.movieswanticketapp.entity.Question;
+import com.nhn.rookie8.movieswanticketapp.entity.Reservation;
 import com.nhn.rookie8.movieswanticketapp.repository.MovieScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 @Service
 @Log4j2
@@ -20,30 +24,28 @@ public class MovieScheduleServiceImpl implements MovieScheduleService {
     private final MovieScheduleRepository repository;
 
     @Override
-    public void registerMovieSchedule(MovieScheduleInpDTO dto) {
-        MovieSchedule entity = dtoToEntity(dto);
+    public void registerMovieSchedule(MovieScheduleInputDTO movieScheduleInputDTO) {
+        MovieSchedule entity = dtoToEntity(movieScheduleInputDTO);
         repository.save(entity);
     }
 
     @Override
-    public MovieScheduleDTO findMovieSchedule(String tid) {
-        Optional<MovieSchedule> result = repository.findById(tid);
+    public void deleteMovieSchedule(String timeTableId) {
+        repository.deleteById(timeTableId);
+    }
+
+    @Override
+    public MovieScheduleDTO getASchedule(String timeTableId) {
+        Optional<MovieSchedule> result = repository.findById(timeTableId);
         return result.isPresent() ? entityToDTO(result.get()) : null;
     }
 
     @Override
-    public void deleteMovieSchedule(String tid) {
-        repository.deleteById(tid);
-    }
-
-    @Override
-    public List<MovieScheduleDTO> getMovieSchedule(String mid) {
+    public List<MovieScheduleDTO> getAllSchedulesOfMovie(String mid) {
         List<MovieSchedule> result = repository.findByMidOrderByTimeAsc(mid);
-        List<MovieScheduleDTO> scheduleList = new ArrayList<MovieScheduleDTO> ();
+        List<MovieScheduleDTO> schedulesList = new ArrayList<> ();
 
-        result.forEach(e -> {
-            scheduleList.add(entityToDTO(e));
-        });
-        return scheduleList;
+        result.forEach(e -> schedulesList.add(entityToDTO(e)));
+        return schedulesList;
     }
 }
