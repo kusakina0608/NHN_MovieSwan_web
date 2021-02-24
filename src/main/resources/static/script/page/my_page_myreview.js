5b1789827bc1303293a2b28aa9cf20198693768c
박경원 <pkw1012@nhn.com>
2021-02-24 09:36:49 +0900

    [Refactor] My Review에서 제목을 Axios로 가져오도록 변경, 신입-베이스캠프-8기-루키백조TF/95

--- src/main/resources/static/script/page/my_page_myreview.js
+++ src/main/resources/static/script/page/my_page_myreview.js
@@ -3,4 +3,22 @@
 (function() {
     const menu = document.getElementById("menu_review");
     menu.className = "active";
+
+    const requestTicketAPI = axios.create({
+        baseURL: location.origin
+    });
+
+    const movieAPI = {
+        getTitle: (mid) => {
+            return requestTicketAPI.get(`/api/movie/getTitle?mid=${mid}`);
+        }
+    }
+
+    var reviews = document.querySelectorAll(".review_title");
+    reviews.forEach(review => {
+        var mid = review.querySelector("input");
+        movieAPI.getTitle(mid).then(response => {
+            review.querySelector(".title").innerText = response.data.name;
+        })
+    })
 } ());
