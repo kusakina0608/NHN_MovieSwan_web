2d12d9eebd3eb05d248d8b8abb3ea04191fc4991
박경원 <pkw1012@nhn.com>
2021-02-25 11:45:53 +0900

    [Feature] CLI를 통한 예매 상세 내역 조회, #신입-베이스캠프-8기-루키백조TF/100

--- src/main/java/com/nhn/rookie8/movieswanticketapp/controller/CliController.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/controller/CliController.java
@@ -6,11 +6,13 @@ import com.nhn.rookie8.movieswanticketapp.dto.ReservationDTO;
 import com.nhn.rookie8.movieswanticketapp.entity.Reservation;
 import com.nhn.rookie8.movieswanticketapp.service.ReservationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
 @ShellComponent
+@Log4j2
 @RequiredArgsConstructor
 public class CliController {
     private final ReservationService service;
@@ -20,9 +22,27 @@ public class CliController {
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
         PageResultDTO<ReservationDTO, Reservation> result = service.getMyReservationList(pageRequestDTO, uid);
         StringBuilder message = new StringBuilder();
+
+        if(result.getDtoList().isEmpty())
+            return "예매 내역이 없습니다.";
+
+        message.append("예매 번호\t\t영화 제목\t\t\t상영 일자\t예매 일자\n");
         for (ReservationDTO reservationDTO : result.getDtoList())
             message.append(service.getReservationInfo(reservationDTO));
 
         return message.toString();
+    }
+
+    @ShellMethod("예매내역 상세조회")
+    public String dtl(@ShellOption String uid, @ShellOption String rid) {
+        ReservationDTO reservationDTO = service.getReservation(rid);
+
+        if(reservationDTO == null || !reservationDTO.getUid().equals(uid)) {
+            log.warn("reservation : {}", reservationDTO);
+            log.warn("uid : {}", uid);
+            return "잘못된 예약 번호입니다.";
+        }
+
+        return service.getReservationDetail(reservationDTO);
     }
 }
--- src/main/java/com/nhn/rookie8/movieswanticketapp/service/ReservationServiceImpl.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/service/ReservationServiceImpl.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Service;
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 import java.util.function.Function;
@@ -106,14 +107,32 @@ public class ReservationServiceImpl implements ReservationService {
         info.append(reservation.getRid() + "\t");
         info.append(title + "\t");
         info.append(startTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + "\t");
-        info.append(reservation.getRegDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + "\n");
+        info.append(reservation.getRegDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "\n");
 
         return info.toString();
     }
 
     @Override
     public String getReservationDetail(ReservationDTO reservation) {
-        return null;
+        StringBuilder detail = new StringBuilder();
+        String title = repository.getMovieName(reservation.getTid());
+        LocalDateTime startTime = repository.getStartTime(reservation.getTid());
+        List<String> seats = repository.getSeatList(reservation.getRid());
+
+        detail.append("예약 번호 : " + reservation.getRid() + "\n");
+        detail.append("영화 제목 : " + title + "\n");
+        detail.append("상영 일자 : " + startTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + "\n");
+        detail.append("예약 일자 : " + reservation.getRegDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "\n");
+        detail.append("좌석 정보 : ");
+        for(String seat : seats)
+            detail.append(seat + " ");
+        detail.append("\n결제 정보\n");
+        detail.append("성인 : " + reservation.getAdultNum() + "\t");
+        detail.append("청소 : " + reservation.getChildNum() + "\t");
+        detail.append("우대 : " + reservation.getOldNum() + "\n");
+        detail.append("총 결제 금액 : " + reservation.getPrice());
+
+        return detail.toString();
     }
 
     private BooleanBuilder getUserInfo(String uid) {
