f3aa4301dd28ce2d9dc0bb251dae6f56db62b8c5
김현철 <supergrammer@naver.com>
2021-02-26 16:59:38 +0900

    [Feat] MvcConfiguration 추가, #신입-베이스캠프-8기-루키백조TF/10

---
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/interceptor/MvcConfiguration.java
@@ -0,0 +1,23 @@
+package com.nhn.rookie8.movieswanticketapp.interceptor;
+
+import com.nhn.rookie8.movieswanticketapp.interceptor.UserAuthInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    // Pattern 추가로 검사할 영역을 지정할 수 있습니다.
+    private final List<String> pattern = new ArrayList<>(Arrays.asList("/mypage", "/movie"));
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UserAuthInterceptor())
+                .addPathPatterns(pattern);
+    }
+}
