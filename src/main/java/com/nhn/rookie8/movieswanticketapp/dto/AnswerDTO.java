b5f40be27a185ed36261318ef7aee9cf17a1abda
김현철 <supergrammer@naver.com>
2021-03-01 16:59:50 +0900

    [Feat] 1:1 문의 답변 DTO 추가, #신입-베이스캠프-8기-루키백조TF/90

---
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/dto/AnswerDTO.java
@@ -0,0 +1,19 @@
+package com.nhn.rookie8.movieswanticketapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnswerDTO {
+    private Integer questionId;
+    private String content;
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
+}
