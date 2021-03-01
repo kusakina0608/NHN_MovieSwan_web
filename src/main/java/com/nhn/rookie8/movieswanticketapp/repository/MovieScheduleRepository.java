1a04fb652f3760ce82db3cfdcbe4cf3ff6ef8578
김현철 <supergrammer@naver.com>
2021-03-01 17:09:32 +0900

    [Feat] 영화 시간표 리포지토리 함수명 변경, #신입-베이스캠프-8기-루키백조TF/88

--- src/main/java/com/nhn/rookie8/movieswanticketapp/repository/MovieScheduleRepository.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/repository/MovieScheduleRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MovieScheduleRepository extends JpaRepository<MovieSchedule, String> {
-    List<MovieSchedule> findByMidOrderByTimeAsc(String mid);
+    List<MovieSchedule> findByMovieIdOrderByStartTimeAsc(String mid);
 }
