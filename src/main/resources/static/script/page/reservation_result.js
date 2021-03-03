dd95a15c8c1c47e3147cb74df4a41ce5fa3b7686
조준호 <junocho21@gmail.com>
2021-03-03 18:20:39 +0900

    [fix] 예매 메세지전송 리팩토링

--- src/main/resources/static/script/page/reservation_result.js
+++ src/main/resources/static/script/page/reservation_result.js
@@ -1,14 +1,19 @@
 'use strict';
 
 (function() {
-    emailjs.init('user_HWJSjiu6Yqh1TaX3CYQHR');
-    var templateParams = {
-        movie_title,
-        movie_date,
-        movie_time,
-        movie_theater,
-        user_name,
-        user_url
-    }
-    emailjs.send('service_x131zev','template_21e1e7g',templateParams);
+
+    // axios.post(url, {
+    //     "botName": "MovieSwan",
+    //     "botIconImage": "https://translate.nhnent.com/icon/botimage.jpg",
+    //     "text": "MovieSwan 본인확인",
+    //     "attachments": 
+    //     [
+    //         {
+    //             "title": "MovieSwan 본인확인",
+    //             "titleLink": null,
+    //             "text": `인증번호 [${authString}] 을 입력해주세요.`
+    //         }
+    //     ]
+    // });
+
 }());
