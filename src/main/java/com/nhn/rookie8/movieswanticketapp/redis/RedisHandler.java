694da2700303b7672af647d4c937c69ebdba264a
김현철 <supergrammer@naver.com>
2021-03-05 16:48:05 +0900

    [Feat] Redis 핸들러 구현, #신입-베이스캠프-8기-공지사항/83

---
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/redis/RedisHandler.java
@@ -0,0 +1,40 @@
+package com.nhn.rookie8.movieswanticketapp.redis;
+
+import com.nhn.rookie8.movieswanticketapp.dto.MemberIdNameDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class RedisHandler {
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+
+    public void saveMemberInfo(String key, MemberIdNameDTO member) {
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key, member, Duration.ofMinutes(30));
+    }
+
+    public void expireAuth(String key) {
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key, null, Duration.ofMillis(1));
+    }
+
+    public MemberIdNameDTO readMemberInfo(String key) {
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        MemberIdNameDTO memberId = (MemberIdNameDTO) valueOperations.get(key);
+        return memberId;
+    }
+
+    public boolean validMemberInfo (String key) {
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        return key != null && valueOperations.get(key) != null;
+    }
+}
