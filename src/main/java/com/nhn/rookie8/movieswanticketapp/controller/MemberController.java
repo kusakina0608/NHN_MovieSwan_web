f919a43eea3ab879f8efb31cb6fcb77c80f5e586
김현철 <supergrammer@naver.com>
2021-03-05 18:00:10 +0900

    [Refactor] 필요 없는 구문 삭제

--- src/main/java/com/nhn/rookie8/movieswanticketapp/controller/MemberController.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/controller/MemberController.java
@@ -19,7 +19,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/member")
