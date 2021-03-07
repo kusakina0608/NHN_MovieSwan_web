5bd3424491a228548d99e6193ccb9da06e0224f9
김현철 <supergrammer@naver.com>
2021-03-07 17:37:28 +0900

    [Feat] 상영 시간표 조건 추가, #신입-베이스캠프-8기-루키백조TF/88

--- src/main/java/com/nhn/rookie8/movieswanticketapp/service/TimetableServiceImpl.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/service/TimetableServiceImpl.java
@@ -2,12 +2,18 @@ package com.nhn.rookie8.movieswanticketapp.service;
 
 import com.nhn.rookie8.movieswanticketapp.dto.TimetableDTO;
 import com.nhn.rookie8.movieswanticketapp.dto.TimetableInputDTO;
+import com.nhn.rookie8.movieswanticketapp.entity.QTimetable;
 import com.nhn.rookie8.movieswanticketapp.entity.Timetable;
 import com.nhn.rookie8.movieswanticketapp.repository.TimetableRepository;
+import com.querydsl.core.BooleanBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -65,7 +71,10 @@ public class TimetableServiceImpl implements TimetableService {
     @Override
     public List<TimetableDTO> getAllTimetableOfMovie(String movieId) {
         try {
-            List<Timetable> result = timetableRepository.findByMovieIdOrderByStartTimeAsc(movieId);
+            BooleanBuilder booleanBuilder = getUnScreened(movieId);
+            Pageable pageable = PageRequest.of(0, 1000, Sort.by("startTime").ascending());
+
+            List<Timetable> result = timetableRepository.findAll(booleanBuilder, pageable).toList();
             List<TimetableDTO> schedulesList = new ArrayList<>();
 
             log.info("Search Results : {}", result);
@@ -76,5 +85,15 @@ public class TimetableServiceImpl implements TimetableService {
             log.error(e);
             return Collections.emptyList();
         }
+    }
+
+    private BooleanBuilder getUnScreened(String movieId) {
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QTimetable qTimetable = QTimetable.timetable;
+
+        booleanBuilder.and(qTimetable.movieId.eq(movieId));
+        booleanBuilder.and(qTimetable.startTime.gt(LocalDateTime.now()));
+
+        return booleanBuilder;
     }
 }
