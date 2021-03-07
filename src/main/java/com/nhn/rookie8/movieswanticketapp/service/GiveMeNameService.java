7c9edf63a952e1b4c39285c78ef6b0c7b80ec693
김현철 <supergrammer@naver.com>
2021-03-07 17:01:47 +0900

    [Feat] DB 인증 서비스 추가, #신입-베이스캠프-8기-루키백조TF/102

---
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/service/GiveMeNameService.java
@@ -0,0 +1,25 @@
+package com.nhn.rookie8.movieswanticketapp.service;
+
+import com.nhn.rookie8.movieswanticketapp.dto.MemberIdNameDTO;
+import com.nhn.rookie8.movieswanticketapp.entity.Auth;
+
+public interface GiveMeNameService {
+
+    public void saveMemberInfo(String authKey, MemberIdNameDTO member);
+
+    public void expireAuth(String authKey);
+
+    public MemberIdNameDTO readMemberInfo(String authKey);
+
+    public boolean validMemberInfo(String authKey);
+
+    public void deleteExpiredAuthKey();
+
+    default Auth toEntity(String key, MemberIdNameDTO member) {
+        return Auth.builder()
+                .authKey(key)
+                .memberId(member.getMemberId())
+                .name(member.getName())
+                .build();
+    }
+}
