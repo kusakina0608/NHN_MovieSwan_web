6582d05b9a84e4e3865b6ec49764b186edec9452
김현철 <supergrammer@naver.com>
2021-03-11 15:43:53 +0900

    [Feat] 에러 처리 추가, #신입-베이스캠프-8기-루키백조TF/87

---
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/ticketexception/SessionExpiredErrorException.java
@@ -0,0 +1,7 @@
+package com.nhn.rookie8.movieswanticketapp.ticketexception;
+
+public class SessionExpiredErrorException extends RuntimeException {
+    public SessionExpiredErrorException(){
+        super();
+    }
+}
