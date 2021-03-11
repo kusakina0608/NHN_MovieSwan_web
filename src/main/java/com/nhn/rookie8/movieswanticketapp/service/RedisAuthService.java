df03f7dfe62f6e6d23a8581191c611e3a90c3b91
김현철 <supergrammer@naver.com>
2021-03-11 10:54:28 +0900

    [Refactor] RedisAuth 서비스 생성자 주입 시점 변경, #신입-베이스캠프-8기-루키백조TF/87

--- src/main/java/com/nhn/rookie8/movieswanticketapp/service/RedisAuthService.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/service/RedisAuthService.java
@@ -1,46 +1,75 @@
 package com.nhn.rookie8.movieswanticketapp.service;
 
 import com.nhn.rookie8.movieswanticketapp.dto.MemberIdNameDTO;
-import com.nhn.rookie8.movieswanticketapp.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.time.Duration;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 @ConditionalOnProperty(name="auth", havingValue = "redis", matchIfMissing = true)
 public class RedisAuthService implements AuthService {
     @Autowired
     RedisTemplate<String, Object> redisTemplate;
 
-    @Override
-    public void saveMemberInfo(String key, MemberIdNameDTO member) {
-        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
-        valueOperations.set(key, member, Duration.ofMinutes(30));
+    private ValueOperations<String, Object> valueOps;
+
+    @PostConstruct
+    public void initValueOps() {
+        valueOps = redisTemplate.opsForValue();
     }
 
     @Override
-    public void expireAuth(String key) {
-        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
-        valueOperations.set(key, null, Duration.ofMillis(1));
+    public void saveMemberInfo(String authKey, MemberIdNameDTO member) {
+        try {
+            valueOps.setIfAbsent(authKey, member, Duration.ofMinutes(30));
+
+            log.info("Registered AuthKey : {}", authKey);
+        } catch (Exception e) {
+            log.error(e);
+        }
     }
 
     @Override
-    public MemberIdNameDTO readMemberInfo(String key) {
-        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
-        MemberIdNameDTO member = (MemberIdNameDTO) valueOperations.get(key);
-        return member;
+    public void expireAuth(String authKey) {
+        try {
+            valueOps.setIfPresent(authKey, null, Duration.ofMillis(1));
+
+            log.info("Expired AuthKey : {}", authKey);
+        } catch (Exception e) {
+            log.error(e);
+        }
     }
 
     @Override
-    public boolean validMemberInfo(String key) {
-        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
-        return key != null && valueOperations.get(key) != null;
+    public MemberIdNameDTO readMemberInfo(String authKey) {
+        try {
+            MemberIdNameDTO member = (MemberIdNameDTO) valueOps.get(authKey);
+
+            log.info("Member Info : {}", member != null ? member : "No Result");
+
+            return member;
+        } catch (Exception e) {
+            log.error(e);
+            return null;
+        }
+    }
+
+    @Override
+    public boolean validMemberInfo(String authKey) {
+        try {
+            return authKey != null && valueOps.get(authKey) != null;
+        } catch (Exception e) {
+            log.error(e);
+            return false;
+        }
     }
 }
