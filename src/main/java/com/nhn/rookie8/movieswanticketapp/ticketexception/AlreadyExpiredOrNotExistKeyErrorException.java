62823683ce1a921698882563cf3df3411e33a2ca
김현철 <supergrammer@naver.com>
2021-03-12 13:30:12 +0900

    [Refactor] 예외처리 클래스, 컨트롤러 삭제, #신입-베이스캠프-8기-루키백조TF/102

--- src/main/java/com/nhn/rookie8/movieswanticketapp/ticketexception/AlreadyExpiredOrNotExistKeyErrorException.java
+++
@@ -1,7 +0,0 @@
-package com.nhn.rookie8.movieswanticketapp.ticketexception;
-
-public class AlreadyExpiredOrNotExistKeyErrorException extends RuntimeException {
-    public AlreadyExpiredOrNotExistKeyErrorException(){
-        super();
-    }
-}
