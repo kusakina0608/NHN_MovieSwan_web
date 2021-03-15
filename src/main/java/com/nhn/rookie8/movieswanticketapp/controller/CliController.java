6b998f4812640267002193472247f1f91bb8b7de
박경원 <pkw1012@nhn.com>
2021-03-15 15:17:24 +0900

    [Refactor] 데이터 가공을 dto 내부에서 하도록 변경

--- src/main/java/com/nhn/rookie8/movieswanticketapp/controller/CliController.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/controller/CliController.java
@@ -29,13 +29,8 @@ public class CliController {
 
         StringBuilder message = new StringBuilder();
         message.append("예매 번호\t\t영화 제목\t상영 일자\t\t예매 일자\n");
-        for (ReservationDetailDTO reservationDTO : result.getDtoList()) {
-            message.append(reservationDTO.getReservationId()).append("\t")
-                    .append(reservationDTO.getTitle()).append("\t")
-                    .append(reservationDTO.getStartTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))).append("\t")
-                    .append(reservationDTO.getPayDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))).append("\t")
-                    .append("\n");
-        }
+        for (ReservationDetailDTO reservationDTO : result.getDtoList())
+            message.append(reservationDTO.toSimpleString()).append("\n");
 
         return message.toString();
     }
@@ -50,21 +45,7 @@ public class CliController {
             return "잘못된 예약 번호입니다.";
         }
 
-        StringBuilder message = new StringBuilder();
-        message.append("예약 번호 : ").append(reservationId).append("\n")
-                .append("영화 제목 : ").append(reservationDTO.getTitle()).append("\n")
-                .append("시작 시간 : ").append(reservationDTO.getStartTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))).append("\n")
-                .append("예약 일자 : ").append(reservationDTO.getPayDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))).append("\n")
-                .append("좌석 정보 : ");
-        for(String seat : reservationDTO.getSeats())
-            message.append(seat).append(" ");
-        message.append("\n").append("결제 정보").append("\n")
-                .append("성인 : ").append(reservationDTO.getAdultNum()).append("\t")
-                .append("청소년 : ").append(reservationDTO.getYoungNum()).append("\t")
-                .append("우대 : ").append(reservationDTO.getElderNum()).append("\n")
-                .append("총 결제 금액 : ").append(reservationDTO.getPrice());
-
-        return message.toString();
+        return reservationDTO.toDetailedString();
     }
 
     private boolean isValidReservation(String memberId, ReservationDetailDTO reservationDTO) {
--- src/main/java/com/nhn/rookie8/movieswanticketapp/dto/ReservationDetailDTO.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/dto/ReservationDetailDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Data
@@ -25,4 +26,24 @@ public class ReservationDetailDTO {
     private List<String> seats;
     private LocalDateTime startTime;
     private LocalDateTime payDate;
+
+    public String toSimpleString() {
+        return reservationId + "\t"
+                + title + "\t"
+                + startTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + "\t"
+                + payDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    public String toDetailedString() {
+        return "예약 번호 : " + reservationId + "\n"
+                + "영화 제목 : " + title + "\n"
+                + "시작 시간 : " + startTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) + "\n"
+                + "예약 일자 : " + payDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "\n"
+                + "좌석 정보 : " + seats.toString() + "\n"
+                + "결제 정보\n"
+                + "성인 : " + adultNum + "\t"
+                + "청소년 : " + youngNum + "\t"
+                + "우대 : " + elderNum + "\n"
+                + "총 결제 금액 : " + price;
+    }
 }
