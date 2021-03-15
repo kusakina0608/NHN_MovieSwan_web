29322f827d4492d808182d240ff97b012ee6b311
김현철 <supergrammer@naver.com>
2021-03-15 16:56:41 +0900

    [Feat] TokenResponseDTO 추가, #신입-베이스캠프-8기-루키백조TF/109

---
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/dto/TokenResponseDTO.java
@@ -0,0 +1,14 @@
+package com.nhn.rookie8.movieswanticketapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenResponseDTO {
+    private String url;
+}
