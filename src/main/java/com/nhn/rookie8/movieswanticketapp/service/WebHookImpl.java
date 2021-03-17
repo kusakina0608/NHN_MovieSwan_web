1e7d1556a7f8daf40b83f9f20a68ee06cd4de114
김현철 <supergrammer@naver.com>
2021-03-17 13:33:27 +0900

    [Feat] 예매 취소 WebHook 구현체 구현,  #신입-베이스캠프-8기-루키백조TF/110

--- src/main/java/com/nhn/rookie8/movieswanticketapp/service/WebHookImpl.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/service/WebHookImpl.java
@@ -6,6 +6,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 @Service
@@ -46,7 +48,38 @@ public class WebHookImpl implements WebHook {
     }
 
     @Override
-    public void sendReservationCanceledMessage(MemberDTO memberDTO, String movieTitle) {
+    public void sendReservationCanceledMessage(MemberDTO memberDTO, ReservationDetailDTO reservationDetailDTO) {
+        StringBuilder titleMsg = new StringBuilder();
+        StringBuilder textMsg = new StringBuilder();
 
+        ReservationAlarmDTO reservationAlarmDTO = ReservationAlarmDTO.builder()
+                .botName("Movie Swan")
+                .botIconImage("https://daejin-lee.github.io/assets/profile.png")
+                .text("예매가 취소되었습니다.")
+                .attachments(new ArrayList<>())
+                .build();
+        titleMsg.append(memberDTO.getName())
+                .append("님, ")
+                .append(reservationDetailDTO.getTitle())
+                .append(" 의 예매가 취소되었습니다.");
+        textMsg.append(memberDTO.getName())
+                .append("님 안녕하세요, 영화 ")
+                .append(reservationDetailDTO.getTitle())
+                .append(" 의 예매 취소가 완료되었습니다\n예매 번호 : ")
+                .append(reservationDetailDTO.getReservationId())
+                .append("\n취소 시간 : ")
+                .append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+        reservationAlarmDTO.getAttachments().add(
+                AttachmentDTO.builder()
+                        .title(titleMsg.toString())
+                        .titleLink("dev-movieswan.nhn.com/")
+                        .text(textMsg.toString())
+                        .color("red")
+                        .build()
+        );
+
+        log.info("memberDTO.getUrl(): {}", memberDTO.getUrl());
+        restTemplate.postForObject(memberDTO.getUrl(), reservationAlarmDTO, ReservationAlarmDTO.class);
     }
 }
