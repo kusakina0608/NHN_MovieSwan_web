5d464194f8e242f83b2e3d66cd52110b18f48afb
조준호 <junocho21@gmail.com>
2021-03-19 19:08:05 +0900

    [refacf] configuration 코드 리팩토링 , #신입-베이스캠프-8기-루키백조TF/140

--- src/main/java/com/nhn/rookie8/movieswanticketapp/MovieswanTicketAppApplication.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/MovieswanTicketAppApplication.java
@@ -1,7 +1,9 @@
 package com.nhn.rookie8.movieswanticketapp;
 
+import com.nhn.rookie8.movieswanticketapp.configuration.ExternalLoginConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +12,7 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableConfigurationProperties(ExternalLoginConfiguration.class)
 public class MovieswanTicketAppApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/configuration/ExternalLoginConfiguration.java
@@ -0,0 +1,19 @@
+package com.nhn.rookie8.movieswanticketapp.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+import java.util.Map;
+
+
+@Configuration
+@Order(value=6)
+@Data
+@ConfigurationProperties(prefix = "external.login")
+public class ExternalLoginConfiguration{
+
+    private Map<String, String> externalUrl;
+}
