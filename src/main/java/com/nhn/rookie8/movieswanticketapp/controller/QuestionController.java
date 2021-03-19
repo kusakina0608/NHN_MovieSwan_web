033a1da12d2e2e980aa567e02f28ae651ae1e335
김현철 <supergrammer@naver.com>
2021-03-19 18:10:01 +0900

    [Feat] 답변 조회, 등록 관련 컨트롤러 추가, #신입-베이스캠프-8기-루키백조TF/90

--- src/main/java/com/nhn/rookie8/movieswanticketapp/controller/QuestionController.java
+++ src/main/java/com/nhn/rookie8/movieswanticketapp/controller/QuestionController.java
@@ -1,17 +1,13 @@
 package com.nhn.rookie8.movieswanticketapp.controller;
 
-import com.nhn.rookie8.movieswanticketapp.dto.PageRequestDTO;
-import com.nhn.rookie8.movieswanticketapp.dto.PageResultDTO;
-import com.nhn.rookie8.movieswanticketapp.dto.QuestionDTO;
+import com.nhn.rookie8.movieswanticketapp.dto.*;
 import com.nhn.rookie8.movieswanticketapp.entity.Question;
+import com.nhn.rookie8.movieswanticketapp.service.AnswerService;
 import com.nhn.rookie8.movieswanticketapp.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/question")
 @Controller
@@ -20,21 +16,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Log4j2
 public class QuestionController {
-    private final QuestionService service;
+    private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/register")
     public String registerQuestion(QuestionDTO questionDTO) {
-        service.registerQuestion(questionDTO);
+        questionService.registerQuestion(questionDTO);
         return "redirect:/mypage/question";
     }
 
     @GetMapping("/post")
     public QuestionDTO readQuestion(@RequestParam Integer qid) {
-        return service.readQuestion(qid);
+        return questionService.readQuestion(qid);
     }
 
     @GetMapping("/getall")
     public PageResultDTO<QuestionDTO, Question> getAllQuestionList(PageRequestDTO pageRequestDTO) {
-        return service.getAllQuestionList(pageRequestDTO);
+        return questionService.getAllQuestionList(pageRequestDTO);
+    }
+
+    @PostMapping("/answer")
+    public String registerAnswer(AnswerDTO answerDTO) {
+        answerService.registerAnswer(answerDTO);
+        return "redirect:/admin/question";
+    }
+
+    @ResponseBody
+    @GetMapping("/isAnswered")
+    public boolean isAnswered(@RequestParam Integer questionId) {
+        return answerService.isAnswered(questionId);
+    }
+
+    @ResponseBody
+    @GetMapping("/getAnswer")
+    public AnswerDTO getAnswer(@RequestParam Integer questionId) {
+        return answerService.readAnswer(questionId);
     }
 }
