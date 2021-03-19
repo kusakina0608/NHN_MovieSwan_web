93e3aba662b079f99900e166133c6d80156cfa0f
김현철 <supergrammer@naver.com>
2021-03-19 18:12:18 +0900

    [Feat] 답변 상태 스크립트 추가, #신입-베이스캠프-8기-루키백조TF/90

--- src/main/resources/static/script/page/my_page_question.js
+++ src/main/resources/static/script/page/my_page_question.js
@@ -8,3 +8,35 @@ function trClicked(value) {
     const menu = document.getElementById("menu_question");
     menu.className = "active";
 } ());
+
+(function() {
+    const requestTicketAPI = axios.create({
+        baseURL: location.origin
+    });
+
+    const answerAPI = {
+        isAnswered: (questionId) => {
+            return requestTicketAPI.get(`/api/question/isAnswered?questionId=${questionId}`);
+        }
+    }
+
+    const table = document.querySelectorAll("tr");
+
+    for (let i = 1; i < table.length; i++) {
+        const questionId = table[i].querySelector("td").innerText;
+
+        answerAPI.isAnswered(questionId).then(res => {
+            const answered = res.data;
+
+            console.log(answered);
+
+            if (answered) {
+                const complete = table[i].querySelector("#yes");
+                complete.style.display = "block";
+            } else {
+                const waiting = table[i].querySelector("#no");
+                waiting.style.display = "block";
+            }
+        })
+    }
+} ());
